017e07892df4db86120fa1f4c6c1c2d4ae72b1d6
Tarik-3 <tarikaitahmid@gmail.com>
2024-10-16 11:17:30 +0100

    Running an instance in aws

--- main.tf
+++ main.tf
@@ -1,39 +1,49 @@
 
-# provider 'aws' {
-#     region = 
-# }
+provider "aws" {
+    region = "us-east-1"
+    profile = "tarik"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+
+  tags  = {
+    Name= "terrafom_example"
+  }
+}
 
 # resource "aws_instance" "exemple" {
 #     ami = ""
 #     instance_type = "t2.micro"
 # }
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.22.0"  # Specify the version you want to use
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "~> 2.22.0"  # Specify the version you want to use
+#     }
+#   }
+# }
 
-provider "docker" {
-  host = "npipe:////./pipe/docker_engine"  # Use this for Windows
+# provider "docker" {
+#   host = "npipe:////./pipe/docker_engine"  # Use this for Windows
 
-  # If using Linux/macOS, replace with:
-  # host = "unix:///var/run/docker.sock"
-}
+#   # If using Linux/macOS, replace with:
+#   # host = "unix:///var/run/docker.sock"
+# }
 
-# Pull a Docker image
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
+# # Pull a Docker image
+# resource "docker_image" "nginx" {
+#   name = "nginx:latest"
+# }
 
-# Create a Docker container
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "nginx_container"
-  ports {
-    internal = 80
-    external = 8081
-  }
-}
+# # Create a Docker container
+# resource "docker_container" "nginx" {
+#   image = docker_image.nginx.image_id
+#   name  = "nginx_container"
+#   ports {
+#     internal = 80
+#     external = 8081
+#   }
+# }
